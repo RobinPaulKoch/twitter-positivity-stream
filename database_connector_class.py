63a1f5faa64bdeacbe598de9251ebba961515d2c
Robin Koch <robinkoch994@gmail.com>
2021-05-09 09:37:25 +0200

    Initial stream capability from tweepy API

--- database_connector_class.py
+++ database_connector_class.py
@@ -4,15 +4,15 @@ from mysql.connector import connect, Error
 
 class MySQLConnection:
     def __init__(self):
-        self.host="localhost",
-        self.user=input("Enter username: "),
+        self.host="localhost"
+        self.user=input("Enter username: ")
         self.password=getpass("Enter password: ")
 
     def test_connect(self):
         try:
             with connect(
                 host="localhost",
-                user=self.user[0],
+                user=self.user,
                 password=self.password,
             ) as connection:
                 print(f"achieved to connect with {connection}!")
@@ -23,7 +23,7 @@ class MySQLConnection:
         try:
             with connect(
                 host="localhost",
-                user=self.user[0],
+                user=self.user,
                 password=self.password,
             ) as connection:
                 with connection.cursor() as cursor:

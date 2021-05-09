f2880fd1f3f11815410cd41754a7d2d5e69eabc4
RobinPaulKoch <robinkoch994@gmail.com>
2021-05-09 20:38:20 +0200

    Added documentation

--- database_connector_class.py
+++ database_connector_class.py
@@ -6,6 +6,32 @@ import pymysql
 import cryptography
 
 class MySQLConnection:
+    """
+    A class that manages the connection with the given Database.
+
+    ...
+
+    Attributes
+    ----------
+    host : str
+        a string with the role for the user on the DB
+    user : str
+        the username on the DB (could be root)
+    password : str
+        a string with the password to connect to the database
+    dbname : str
+        name of the database we want to connect with
+
+    Methods
+    -------
+    execute(statement, return_result)
+        Executes SQL code on the DB. Returns the result if desired (if=True)
+
+    connect_with_alchemy()
+        Creates an alchemy engine class with the given database.
+
+    """
+
     def __init__(self, dbname='default'):
         self.host="localhost"
         self.user=input("Enter username: ")

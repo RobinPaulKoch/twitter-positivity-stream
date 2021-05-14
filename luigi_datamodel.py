e50d066ab1fbde9dc9126f448843d86792abd1c6
RobinPaulKoch <robinkoch994@gmail.com>
2021-05-14 10:42:17 +0200

    luigi datamodel.py new version

--- luigi_datamodel.py
+++ luigi_datamodel.py
@@ -1,17 +1,52 @@
 import luigi
 import os
+from datetime import datetime
+from luigi.contrib.mysqldb import MySqlTarget
+from keys import  db_user, db_password
+from configurations import config
 
 # for running: python -m luigi --module Luigi_datamodel DataPipeline --local-scheduler
 
 class InsertNewTweets(luigi.Task):
+    """
+    This task is the start of the data pipeline where the data streams in using
+    the Tweepy API. Some database connection parameters are given to find the
+    MySQL target of the script
+    """
+    rundate = luigi.DateParameter(default=datetime.now().date())
+    host = "localhost:3306"
+    db = config.DBNAME
+    target_table = config.TBLNAME
+    user = db_user
+    pw = db_password
 
     def run(self):
-        os.system('python 01_tweepystream.py')
+        os.system('tweepystream.py')
+
+    def get_target(self):
+        return MySqlTarget(host=self.host, database=self.db, user=self.user, password=self.pw, table=self.target_table,
+                           update_id=str(self.rundate))
+
+    def requires(self):
+        return []
+
+    def output(self):
+        return self.get_target()
+
 
 class RankTweets(luigi.Task):
+    """
+    This script takes the input of 'InsertNewTweets' and performs some data
+    manipulation to get the highest ranking tweets based on subjectivity, polarity and
+    emoji count of each hour.
+    """
+
+    def requires(self):
+        return InsertNewTweets()
 
     def run(self):
-        os.system('python 02_rank_tweets.py')
+        os.system('rank_tweets.py')
+
 
 if __name__ == '__main__':
-     luigi.build([InsertNewTweets(), RankTweets()], local_scheduler=True)
+     luigi.build([RankTweets()], workers=1, local_scheduler=True)
